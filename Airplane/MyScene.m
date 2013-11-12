1c0fcb5e071439ec158463f48a7c47e1e06eec10
Christopher Eatough <christophereatough@Christophers-MacBook-Pro.local>
2013-11-12 20:31:58 +0000

    Adding a score and a score node to the main screen - increments every time an enemy plane is shot down

--- Airplane/MyScene.m
+++ Airplane/MyScene.m
@@ -105,11 +105,11 @@
         }
         
         // adding a label that has the players score
-        SKLabelNode *scoreNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        scoreNode.text = @"0";
-        scoreNode.fontSize = 20;
-        scoreNode.position = CGPointMake(0, 0);
-        [self addChild:scoreNode];
+        _scoreNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        _scoreNode.text = [NSString stringWithFormat:@"%d", score];
+        _scoreNode.fontSize = 20;
+        _scoreNode.position = CGPointMake(10, 0);
+        [self addChild:_scoreNode];
         
     }
     return self;
@@ -314,6 +314,9 @@
         SKNode *enemy = (contact.bodyA.categoryBitMask & bulletCategory) ? contact.bodyB.node : contact.bodyA.node;
         [projectile runAction:[SKAction removeFromParent]];
         [enemy runAction:[SKAction removeFromParent]];
+        
+        score++;
+        _scoreNode.text = [NSString stringWithFormat:@"%d", score];
         
         //add explosion
         SKSpriteNode *explosion = [SKSpriteNode spriteNodeWithTexture:[_explosionTextures objectAtIndex:0]];
